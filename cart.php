0a03477b35139069414ed64d3669fb60209db438
Grom69 <jerome.dannfald@orange.fr>
2022-11-05 11:32:54 +0100

    corrections

--- cart.php
+++ cart.php
@@ -7,13 +7,20 @@ require 'inc/data/products.php';
 
 <section class="cookies container-fluid">
     <div class="row">
-        <ul>
-            <?php
-            session_start();
-            foreach ($_SESSION['panier'] as $cookie)
-                echo  '<li>' . $catalog[$cookie]['name'] . '</li>';
-            ?>
-        </ul>
+
+        <?php session_start();
+
+        foreach ($_SESSION['panier']  as $id => $cookie) { ?>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-<?= $cookie; ?>.jpg" alt="<?= $catalog[$cookie]['name']; ?>" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3><?= $catalog[$cookie]['name']; ?></h3>
+                        <p><?= $catalog[$cookie]['description']; ?></p>
+                    </figcaption>
+                </figure>
+            </div>
+        <?php } ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
